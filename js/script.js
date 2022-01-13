3834a7ecce5fbfd296e25294027aee31eab68f6c
mintwab2012 <86166984+hiwot21@users.noreply.github.com>
2022-01-13 17:25:21 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Single line comment
+// Single line comments start with //.
+
+// Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+// Multi-line comment
+// Multi-line comments start with /* and end with */.
+
+// Any text between /* and */ will be ignored by JavaScript.
+console.log("Mintwab");
+console.log("My favorite food is enjera");
